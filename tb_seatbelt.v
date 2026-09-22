e021210fd682a4266ddabfcd68444c0c5ad76cf3
gitamadhaviguduri-glitch <gitamadhavi.guduri@gmail.com>
2026-09-22 15:01:11 +0530

    Add files via upload

---
+++ tb_seatbelt.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/22/2026 02:57:08 PM
+// Design Name: 
+// Module Name: tb_seatbelt
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_seatbelt;
+
+    reg clk = 0, rst_n = 0, ignition = 0, seatbelt_unbuckled = 0;
+    wire warning_buzzer;
+
+    seatbelt #(.TARGET_COUNT(5)) uut (clk, rst_n, ignition, seatbelt_unbuckled, warning_buzzer);
+
+    always #5 clk = ~clk;
+
+    initial begin
+        #15 rst_n = 1;
+        #10 ignition = 1; seatbelt_unbuckled = 1;
+        #60; 
+
+        #10 seatbelt_unbuckled = 0;
+        #20 $finish;
+    end
+
+endmodule
+   
